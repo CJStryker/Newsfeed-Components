90fc2f97194bef837c5665d12b5fcc63d608aaa7
Christopher Jospeh Stryker <chrisjstryker@gmail.com>
2019-10-08 14:09:07 -0400

    Finished MVP

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -9,6 +9,66 @@ let menuItems = [
   'Log Out'
 ];
 
+// Dylan's code
+
+function createMenuComponent() {
+    const menu = document.createElement("div");
+    const menuList = document.createElement("ul");
+
+    menuItems.forEach(links => {
+        const menuItem = document.createElement("li");
+        const menuButton = document.createElement("a");
+
+        menuItem.appendChild(menuButton);
+        menuButton.textContent = `${links}`;
+        menuList.appendChild(menuItem);
+
+    });
+    menu.appendChild(menuList);
+    menu.classList.add("menu");
+
+    const menuButton = document.querySelector(".menu-button");
+    menuButton.addEventListener("click", () => menu.classList.toggle("menu--open"));
+
+    const menuH1 = document.querySelector(".header h1");
+    parentContainer = menuH1.parentNode;
+    parentContainer.insertBefore(menu, menuH1);
+
+    return menu;
+
+}
+createMenuComponent();
+
+// Chris' code
+
+/*function menuCreator(listItem) {
+    const menu = document.createElement('div');
+    const menuList = document.createElement('ul');
+    const menuItem = document.createElement('li');
+    const menuButton = document.createElement('a');
+
+    menu.classList.add('menu');
+    menuButton.classList.add('menu-button');
+
+    menuItem.textContent = listItem;
+
+    menuButton.addEventListener('click', () => {
+        menu.toggle('menu--open');
+    });
+
+    menu.appendChild(menuList);
+    menuList.appendChild(menuItem);
+    menuItem.appendChild(menuButton);
+
+    return menu;
+}
+
+const menu = document.querySelector('.menu-button');
+
+menuItems.forEach((d) => {
+    menu.appendChild(menuCreator(d));
+});*/
+
 /* 
 
   Step 1: Write a function that will create a menu component as seen below:
